b441ef0789363a7ba2d63d34939938a4e739b36c
Coding <Coding@Teds-Air.lan>
2022-12-19 23:15:04 -0600

    created checkout page

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -9,7 +9,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './checkout.styles.scss'
 
 const Checkout = () => {
-    const { cartItems} = useContext(CartContext);
+    const { cartItems, cartTotal} = useContext(CartContext);
     return (
         <div className='checkout-container'>
             <div className='checkout-header'>
@@ -38,7 +38,7 @@ const Checkout = () => {
                         <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
      ))};
 
-         <span className='total'>Total: 0</span>
+         <span className='total'>Total: ${cartTotal}</span>
           
         </div>
     )
